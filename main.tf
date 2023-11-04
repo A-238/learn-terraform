c443c847fd5fd5dca2be02fbd5d53ffabca1689f
A-238 <agafumiya@gmail.com>
2023-11-04 23:37:53 +0900

    AMIからEC2インスタンス起動
    ・t2.microでインスタンス起動

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.24.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_instance" "hello-world" {
+    ami = "ami-098940df4d3292e9a"
+    instance_type = "t2.micro"
+}
