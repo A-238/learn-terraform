fa34e0d19866197f63bb84408b887dd030b5fe6f
A-238 <agafumiya@gmail.com>
2023-11-08 23:16:44 +0900

    ひな型作成

--- main.tf
+++ main.tf
@@ -1,4 +1,8 @@
+# ----------------------------------------------
+# Terraform Config
+# ----------------------------------------------
 terraform {
+  required_version = "1.6.3"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,22 +11,20 @@ terraform {
   }
 }
 
+# ----------------------------------------------
+# Provider
+# ----------------------------------------------
 provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_instance" "hello-world" {
-    ami = "ami-098940df4d3292e9a"
-    instance_type = "t2.micro"
+# ----------------------------------------------
+# Variables
+# ----------------------------------------------
+variable "project" {
+  type = string
+}
 
-    tags = {
-        Name = "HalloWorld"
-    }
-
-    user_data_replace_on_change = true
-    user_data = <<EOF
-#!/bin/bash
-amazon-linux-extras install -y nginx1.12
-systemctl start nginx
-EOF
+variable "environment" {
+  type = string
 }
