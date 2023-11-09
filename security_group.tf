56a86d1e5df7b4c61d25caee188ee3e66142693e
A-238 <agafumiya@gmail.com>
2023-11-09 21:38:11 +0900

    APサーバーのセキュリティグループを作成
    ・S3のプレフィックスリストを読み込み

--- security_group.tf
+++ security_group.tf
@@ -54,6 +54,42 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
+resource "aws_security_group_rule" "app_in_rcp3000" {
+  security_group_id        = aws_security_group.app_sg.id
+  from_port                = 3000
+  protocol                 = "tcp"
+  to_port                  = 3000
+  type                     = "ingress"
+  source_security_group_id = aws_security_group.web_sg.id
+}
+
+resource "aws_security_group_rule" "app_out_http" {
+  security_group_id = aws_security_group.app_sg.id
+  from_port         = 80
+  protocol          = "tcp"
+  to_port           = 80
+  type              = "egress"
+  prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
+}
+
+resource "aws_security_group_rule" "app_out_https" {
+  security_group_id = aws_security_group.app_sg.id
+  from_port         = 443
+  protocol          = "tcp"
+  to_port           = 443
+  type              = "egress"
+  prefix_list_ids   = [data.aws_prefix_list.s3_pl.id]
+}
+
+resource "aws_security_group_rule" "app_out_tcp3306" {
+  security_group_id        = aws_security_group.app_sg.id
+  from_port                = 3306
+  protocol                 = "tcp"
+  to_port                  = 3306
+  type                     = "egress"
+  source_security_group_id = aws_security_group.db_sg.id
+}
+
 # Opmng
 resource "aws_security_group" "opmng_sg" {
   name        = "${var.project}-${var.environment}-opmng-sg"
