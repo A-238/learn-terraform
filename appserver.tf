258df743e97e04068d3a393e47ca476c512e547f
A-238 <agafumiya@gmail.com>
2023-11-12 15:58:32 +0900

    DB接続情報をパラメータストアに作成

--- appserver.tf
+++ appserver.tf
@@ -13,6 +13,40 @@ resource "aws_key_pair" "keypair" {
 }
 
 # ----------------------------------------------
+# SSM Parameter Store
+# ----------------------------------------------
+resource "aws_ssm_parameter" "host" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_HOST"
+  type  = "String"
+  value = aws_db_instance.mysql-standalone.address
+}
+
+resource "aws_ssm_parameter" "port" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_PORT"
+  type  = "String"
+  value = aws_db_instance.mysql-standalone.port
+}
+
+resource "aws_ssm_parameter" "database" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_DATABASE"
+  type  = "String"
+  value = aws_db_instance.mysql-standalone.db_name
+}
+
+resource "aws_ssm_parameter" "username" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_USERNAME"
+  type  = "SecureString"
+  value = aws_db_instance.mysql-standalone.username
+}
+
+resource "aws_ssm_parameter" "password" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_PASSWORD"
+  type  = "SecureString"
+  value = aws_db_instance.mysql-standalone.password
+}
+
+
+# ----------------------------------------------
 # EC2 instance
 # ----------------------------------------------
 resource "aws_instance" "app_server" {
